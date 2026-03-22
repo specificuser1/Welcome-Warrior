8c8a020831e2277864fbb5157e1b8164e0ff1932
specificuser1 <motorolag14rx@gmail.com>
2026-03-22 23:48:58 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,3 @@ load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 WELCOME_CHANNEL_ID = int(os.getenv("WELCOME_CHANNEL_ID"))
-ANTI_RAID_LIMIT = int(os.getenv("ANTI_RAID_LIMIT", 5))
-ANTI_RAID_TIME = int(os.getenv("ANTI_RAID_TIME", 10))
